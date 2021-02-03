7c8d62fd61f7fd44f2ed9b332020d047d717b5ca
Richard Ng <richardkentng@gmail.com>
2021-02-02 16:29:52 -0800

    Gender buttons constrain search success.  API loads in bulk.

--- js/main.js
+++ js/main.js
@@ -1,46 +1,58 @@
-console.log('your insane check');
+
+console.log('------------------sanity check -----------------------');
+
+let currentIndex = null;
+let peopleGbl = [];
+let personGbl = [];
 
 let male = false;
 let either = true;
 let female = false;
 
-//select the following buttons: btn-male, btn-either, btn-female
+
+//store button elements
 const btnMale = document.querySelector('.btn-male')
 const btnEither = document.querySelector('.btn-either')
 const btnFemale = document.querySelector('.btn-female')
 
+const btnNext = document.querySelector('.btn-next')
+const btnPrev = document.querySelector('.btn-prev')
 
-const removeBtnDisabled = () => {
+btnPrev.disabled = true;
+
+
+
+
+
+
+const removeBluesOnGenderButtons = () => {
     btnMale.classList.remove('btn-disabled')
     btnEither.classList.remove('btn-disabled')
     btnFemale.classList.remove('btn-disabled')
 }
 
 const clickMale = () => {
-    console.log('male maine');
     male = true;
     female = false;
     either = false;
-    removeBtnDisabled();
+    removeBluesOnGenderButtons();
     btnFemale.classList.toggle('btn-disabled')
     btnEither.classList.toggle('btn-disabled')
 
 }
 const clickEither = () => {
-    console.log('eithe rman');
     either = true;
     male = false;
     female = false;
-    removeBtnDisabled();
+    removeBluesOnGenderButtons();
     btnMale.classList.toggle('btn-disabled')
     btnFemale.classList.toggle('btn-disabled')    
 }
 const clickFemale = () => {
-    console.log('female man');
     female = true;
     male = false;
     either = false;
-    removeBtnDisabled();
+    removeBluesOnGenderButtons();
     btnMale.classList.toggle('btn-disabled')
     btnEither.classList.toggle('btn-disabled')    
 }
@@ -53,7 +65,7 @@ btnFemale.addEventListener('click',clickFemale)
 // update picture
 const updatePicture = () => {
     const img = document.querySelector('.pic')
-    const newSrcValue = people[0].picture.large
+    const newSrcValue = personGbl.picture.large
     img.setAttribute('src',`${newSrcValue}`)
 }
 
@@ -67,13 +79,11 @@ const updateInfo = () => {
     const phoneEl = document.querySelector('.phone-value');
     //console.log(nameValue, ageValue, locationValue, phoneValue);
 
-    const person = people[0];
-
     //assign user's information to variables
-    const nameValue = `${person.name.first} ${person.name.last}`;
-    const ageValue = `${person.dob.age}`;
-    const locationValue = `${person.location.city}, ${person.location.country}`;
-    const phoneValue = `${person.phone}`
+    const nameValue = `${personGbl.name.first} ${personGbl.name.last}`;
+    const ageValue = `${personGbl.dob.age}`;
+    const locationValue = `${personGbl.location.city}, ${personGbl.location.country}`;
+    const phoneValue = `${personGbl.phone}`
     
     //display information by using accessing textContent
     nameEl.textContent = nameValue
@@ -83,22 +93,107 @@ const updateInfo = () => {
     //console.log(nameValue, ageValue, locationValue, phoneValue);
 }
 
-    
+
+    const filterGender = () => {
+        // console.log('enterFilterGender');
+        // console.log('peopleGbl:', peopleGbl);
+        const peopleGendered = [];
+
+        for (let i = 0; i < peopleGbl.length; i++) {
+            // const msg = male ? "check user for male":"check user for female";
+            // console.log(msg)
+
+            if (male) {
+                if (peopleGbl[i].gender === "male") {
+                    peopleGendered.push(peopleGbl[i]);
+                }
+            } else if (female) {
+                if (peopleGbl[i].gender === "female") {
+                    peopleGendered.push(peopleGbl[i]);
+                }
+            }
+        }   //end for loop 
+
+        if (peopleGendered.length === 0) {
+            console.log('%c peopleGendered array is empty!','color: red; background: black;');
+        }
+
+        peopleGbl = peopleGendered.slice()
+        // for (let i = 0; i < peopleGbl.length; i++) {
+        //     console.log(peopleGbl[i].gender);
+        // }
+    }   //end of function: filterGender
+
+
+
+let remMale = null;
+let remEither = null;
+let remFemale = null;
+
+
     const clickNext = () => {
-        fetch('https://randomuser.me/api/')
-        .then((responseData) => {
-            return responseData.json()
-        })
-        .then((jsonData) => { 
-            people = jsonData.results;
-            console.log(people[0])
-            updatePicture();
-            updateInfo();
-        })
+
+        let sameParamaters = true;
+        if (remMale === null) {
+            remMale = male;
+            remEither = either;
+            remFemale = female;
+        } else {
+            if ((remMale !== male) || (remEither !== either) || (remFemale !== female)) {
+                sameParamaters = false;
+                remMale = male;
+                remEither = either;
+                remFemale = female;                
+            }
+        }
+
+       if ((remMale === null) || (!sameParamaters) || (currentIndex + 1 > peopleGbl.length - 1))
+       {
+            console.log('%c gunna run fetch','color: lime; background: black;');
+
+            fetch('https://randomuser.me/api/?results=50')
+            .then((responseData) => {
+                return responseData.json()
+            })
+            .then((jsonData) => { //THENTEHNTEHNTEHNEHTENETHENTHENETHENTHETNETHENTEHTNEHTEN
+                console.log('%c --------------.then-----------','color: lime; background: black;');
+
+                //get array of people objects
+                peopleGbl = jsonData.results;  
+
+                if (!either) {
+                    filterGender()
+                }
+
+                currentIndex = 0;
+                personGbl = peopleGbl[currentIndex];
+                updatePicture();
+                updateInfo();
+            })  // end .then
+    } else {
+        //search paramaters are the same!  Thre are still more matches in array!
+        currentIndex++;        
+        console.log(`${currentIndex}/${peopleGbl.length - 1}`)
+        personGbl = peopleGbl[currentIndex]
+        updatePicture();
+        updateInfo();
     }
 
 
-    //select btn-next, add listener
-    const btnNext = document.querySelector('.btn-next')
+    }   //end clickNext function
+
+
+    //add listener to btnNext
     btnNext.addEventListener('click',clickNext)
 
+    clickEither()
+    clickNext()
+
+
+
+
+    function z() {
+        console.log('male',remMale);
+        console.log('either',remEither);
+        console.log('female',remFemale);
+    }
